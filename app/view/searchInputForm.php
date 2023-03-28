451ee5c55b6d4ce86ffdf06dbe3043903794efc8
testuser <canda3267@gmail.com>
2023-03-28 22:31:08 +0900

    [fix]パス変更

---
+++ app/view/searchInputForm.php
@@ -0,0 +1,12 @@
+<div class="radio_container form-check-inline">
+    <input class="form-check-input" type="radio" id="radio_1" name="search_target" value="title">
+    <label class="form-check-label" for="radio_1">タイトル</label>
+</div>
+<div class="radio_container form-check-inline">
+    <input class="form-check-input" type="radio" id="radio_2" name="search_target" value="content">
+    <label class="form-check-label" for="radio_2">本文</label>
+</div>
+<div class="radio_container form-check-inline">
+    <input class="form-check-input" type="radio" id="radio_3" name="search_target" value="both" checked>
+    <label class="form-check-label" for="radio_3">両方</label>
+</div>
