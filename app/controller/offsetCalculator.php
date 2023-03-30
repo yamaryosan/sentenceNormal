f94177520b0ad0e66ce5acde2f2e033f8105fa70
testuser <canda3267@gmail.com>
2023-03-30 22:00:20 +0900

    [fix]getメソッドを実装し、処理を正しくした

--- app/controller/offsetCalculator.php
+++ app/controller/offsetCalculator.php
@@ -3,16 +3,24 @@
 /**
  * オフセット値(何番目の検索結果から表示するか)を計算するクラス
  * @param int $currentPage 現在のページ
+ * @param int $displayCount 一度に表示する検索結果の数
+ * @var int $offset オフセット値
  */
 class OffsetCalculator
 {
-    private int $currentPage;
-    function __construct(int $currentPage)
+    private int $currentPage, $displayCount, $offset;
+    function __construct(int $currentPage, int $displayCount)
     {
         $this->currentPage = $currentPage;
+        $this->displayCount = $displayCount;
+        $this->offset = self::calculate();
     }
-    function calculate(int $displayCount)
+    function calculate()
     {
-        return ($this->currentPage - 1) * $displayCount;
+        return ($this->currentPage - 1) * $this->displayCount;
+    }
+    function get(): int
+    {
+        return $this->offset;
     }
 }
