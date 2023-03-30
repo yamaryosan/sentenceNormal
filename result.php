1449950746328006d4e7d5ce032966fdb9399cd1
testuser <canda3267@gmail.com>
2023-03-30 21:51:19 +0900

    [update]リファクタリング

--- result.php
+++ result.php
@@ -2,86 +2,56 @@
 
 declare(strict_types=1);
 
-require_once("./app/classSearchWords.php");
-require_once("./app/classSearcher.php");
-require_once("./app/classCookieSetter.php");
-require_once("./app/classPageNumberCulculator.php");
-require_once("./app/classSearchHistorySaver.php");
+require_once("./app/model/searchWords.php");
+require_once("./app/model/searcher.php");
+require_once("./app/model/searchHistorySaver.php");
+require_once("./app/model/cookieSetter.php");
+require_once("./app/model/searchHistorySaver.php");
+require_once("./app/view/searchRetryView.php");
+require_once("./app/controller/PageCalculator.php");
+require_once("./app/controller/maxDisplayCountCalculator.php");
+require_once("./app/controller/displayCountGetter.php");
+require_once("./app/controller/offsetCalculator.php");
+require_once("./app/controller/currentPageCountGetter.php");
 require_once("./app/classStringHighlighter.php");
 
 // キャッシュを使わせない(クッキーを利用するため)
 header("Cache-Control: no-cache, must-revalidate");
 
-$searchWordString = $_GET["search_word"];
+$searchWordString = $_GET["search_words"];
 $searchWords = new SearchWords($searchWordString);
 $searchWordByComma = $searchWords->getStringByComma();
-$searchTarget = $_GET["search_target"];
+# $searchTarget = $_GET["search_target"];
 
-// 検索語が空の場合やり直しさせる
-if (empty($searchWords->getWords())) {
-    echo "<script>alert('検索語を入力してください。'); history.back();</script>";
-    exit;
-}
+// 文字列が空の場合、検索のやり直しを促す
+retrySearchIfEmpty($searchWords);
 
 // 検索履歴を保存
-// 遷移元が検索ページのときのみ履歴を保存
 $referrer = $_SERVER["HTTP_REFERER"];
-if (is_integer(strpos($referrer, "searchPage.php")) === true) {
-    $searchHistorySaver = new SearchHistorySaver($searchWords);
-    $searchHistorySaver->save();
-} else {
-    // 何もしない
-}
+(new searchHistorySaver($searchWords))->SaveSearchHistoryIfTopPage($referrer);
 
 // 検索ページに戻ったときに備え、クッキーを保存
-$referrer = $_SERVER["HTTP_REFERER"];
-if (is_integer(strpos($referrer, "searchPage.php")) === true) {
-    $cookieSetter = new CookieSetter("search_word_cookie", $searchWordByComma);
-    $cookieSetter->setCookie();
-} else {
-    // 何もしない
-}
+(new CookieSetter("search_words", $searchWordByComma))->set($referrer);
 
 // 検索
-$searcher = new Searcher($searchWords);
-$searchResult = $searcher->search($searchTarget);
+$searchResult = (new Searcher($searchWords))->search();
+# $searchResult = $searcher->search($searchTarget);
 
-// ヒット数があまりに多い場合、やり直させる
-$requestRetryHitNumber = 400;
-if (count($searchResult) > $requestRetryHitNumber) {
-    $alertJSFilePath = "./js/alertMessage.js";
-    $jsCode = "<script src='$alertJSFilePath'></script>;";
-    echo $jsCode;
-}
+// ヒット数が多い場合、やり直させる
+retrySearchIfTooManyResults($searchResult);
 
 // 一度に表示する検索結果の数
-$displayNumber = 0;
-if (isset($_GET["display_number"]) === true) {
-    $displayNumber = (int)$_GET["display_number"];
-} else {
-    $displayNumber = 25;
-}
+$displayCount = (new DisplayCountGetter((int)$_GET["display_count"]))->get();
 
-// ページ数を計算
-$pageNumberCulculator = new PageNumberCulculator($searchResult);
-$totalPages = $pageNumberCulculator->calculate($displayNumber);
-$maxDisplayNumberThisPage = 0;
+// ページネーションのために現在のページ数を取得
+$currentPage = (new CurrentPageCountGetter((int)$_GET["page"]))->get();
 
-// ページネーション
-if (isset($_GET["page"])) {
-    $currentPage = (int)($_GET["page"]);
-} else {
-    $currentPage = 1;
-}
+// 検索結果のオフセットを計算
+$offset = ($currentPage - 1) * $displayCount;
 
-$offset = ($currentPage - 1) * $displayNumber;
-
-$searchResultCurrentCount = count($searchResult) - $offset;
-if ($searchResultCurrentCount < $displayNumber) {
-    $maxDisplayNumberThisPage = $searchResultCurrentCount;
-} else {
-    $maxDisplayNumberThisPage = $displayNumber;
-}
+// 一度に表示する検索結果の数を計算
+$maxDisplayCountThisPage = (new MaxDisplayCountCalculator(count($searchResult), $offset, $displayCount))->get();
+echo $maxDisplayCountThisPage;
 ?>
 
 <!DOCTYPE html>
@@ -96,8 +66,8 @@ if ($searchResultCurrentCount < $displayNumber) {
 <body>
     <div class="main_container">
         <!-- ヒット数 -->
-        <div class="hit_number_text_container">
-            <?php require("./app/functionSearchResultAbstract.php") ?>
+        <div class="hit_count_text_container">
+            <?php require("./app/view/hitCount.php") ?>
         </div>
         <!-- 表示数 -->
         <div class="display_number_container">
@@ -105,7 +75,7 @@ if ($searchResultCurrentCount < $displayNumber) {
         </div>
         <!-- ページネーション -->
         <div class="pagination_container">
-            <?php require("./app/functionPagination.php") ?>
+            <?php require("./app/view/pagination.php") ?>
         </div>
         <!-- 結果 -->
         <div class="results_container">
@@ -113,16 +83,19 @@ if ($searchResultCurrentCount < $displayNumber) {
         </div>
         <!-- ページネーション -->
         <div class="pagination_container">
-            <?php require("./app/functionPagination.php") ?>
+            <?php require("./app/view/pagination.php") ?>
         </div>
+        <!-- ページ最下部の戻るボタン -->
         <div class="page_lower_back_btn_container">
-            <a href="searchPage.php" class="page_lower_back_btn">戻る</a>
+            <a href="top" class="page_lower_back_btn">戻る</a>
         </div>
+        <!-- ページ右下部のトップへ戻るボタン -->
         <div class="back_btn_container">
-            <a href="searchPage.php" class="back_btn">
+            <a href="top" class="back_btn">
                 <img src="./images/backBtnIcon.png">
             </a>
         </div>
+        <!-- ページ右下部の画面トップへ戻るボタン(後で書き換えよう) -->
         <div class="to_top_btn_container">
             <a href="#" class="to_top_btn">
                 <img src="./images/toTopIcon.png">
