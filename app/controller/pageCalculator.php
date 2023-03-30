5cff41b59968423aef1a2134f348a9c98ede8da7
testuser <canda3267@gmail.com>
2023-03-30 22:03:26 +0900

    [add]全結果表示に必要なページ枚数を計算するクラス

---
+++ app/controller/pageCalculator.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * 結果のプレビューぺージの枚数を計算するクラス
+ * 
+ * @param array $searchResult 検索結果
+ * 
+ */
+class PageCalculator
+{
+    private array $searchResult;
+
+    public function __construct(array $searchResult)
+    {
+        $this->searchResult = $searchResult;
+    }
+
+    public function calculate(int $displayCount)
+    {
+        $resultKnowledgeUnitNumber = count($this->searchResult);
+        return (int)(ceil($resultKnowledgeUnitNumber / $displayCount));
+    }
+}
