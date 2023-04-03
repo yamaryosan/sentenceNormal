3e15ec46239f069688286b68e47ad89800034d68
testuser <canda3267@gmail.com>
2023-04-04 08:06:43 +0900

    [update]divタグをこっちに持ってきた

---
+++ app/view/displayCountDecisionView.php
@@ -0,0 +1,11 @@
+<div class="display_count_container">
+    <p>表示数調整</p>
+    <form>
+        <select id="display_number">
+            <option value="25">25</option>
+            <option value="50">50</option>
+            <option value="100">100</option>
+            <option value="200">200</option>
+        </select>
+    </form>
+</div>
