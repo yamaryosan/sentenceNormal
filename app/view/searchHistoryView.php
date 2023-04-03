1950da945243c33aa81bfe02b34c0db49edc758e
testuser <canda3267@gmail.com>
2023-04-03 23:30:13 +0900

    [update]装飾を変更

--- app/view/searchHistoryView.php
+++ app/view/searchHistoryView.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 ?>
 <div class="mt-3 search_history_container">
-    <p>検索履歴</p>
+    <p>検索履歴(押すと検索します)</p>
     <?php foreach ($searchHistory as $history) : ?>
         <div class="mx-auto">
             <a class="search_history_string" href=<?php echo "search.php?search_words=$history" ?>><?php echo $history ?></a>
