681272d2a81285046ddcf88e0df42228dd6c30bf
testuser <canda3267@gmail.com>
2023-04-03 23:40:21 +0900

    [fix]URLを変更しリンクエラーを解消

--- app/view/searchHistoryView.php
+++ app/view/searchHistoryView.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
     <p>検索履歴(押すと検索します)</p>
     <?php foreach ($searchHistory as $history) : ?>
         <div class="mx-auto">
-            <a class="search_history_string" href=<?php echo "search.php?search_words=$history" ?>><?php echo $history ?></a>
+            <a class="search_history_string" href=<?php echo "result?search_words=$history&search_target=both&page=1&display_count=25" ?>><?php echo $history ?></a>
         </div>
     <?php endforeach ?>
 </div>
