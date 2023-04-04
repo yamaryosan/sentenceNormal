edafb07c7794f5dd080edc8ef0c9b155a12e5d56
testuser <canda3267@gmail.com>
2023-04-04 23:32:47 +0900

    [fix]記号マークの記述

--- app/view/pagination.php
+++ app/view/pagination.php
@@ -5,13 +5,18 @@ require_once("./app/controller/totalResultPagesCalculator.php");
 
 $totalPages = (new TotalResultPagesCalculator($searchResult, $displayCount))->get();
 $pageLinkRange = 3;
+$skipPagesStep = 5;
 
 ?>
 
 <div class="pagination_container">
     <?php if ($totalPages >= 2) : ?>
         <?php if ($currentPage !== 1) : ?>
-            <a class="to_first_page" href='<?php echo "result?search_words=$searchWordString&検索開始=送信&page=1&display_count=$displayCount" ?>'>&lt;&lt;</a>
+            <!-- 最後のページへ -->
+            <a class="to_last_page symbol" href='<?php echo "result?search_words=$searchWordString&検索開始=送信&page=1&display_count=$displayCount" ?>'>&lt;&lt;</a>
+            <!-- 複数ページぶん進む -->
+            <?php $skipPagesToLower = max($currentPage - $skipPagesStep, 1) ?>
+            <a class="to_greater symbol" href=<?php echo "result?search_words=$searchWordString&検索開始=送信&page=$skipPagesToLower&display_count=$displayCount" ?>>&lt;</a>
         <?php endif ?>
         <div class="page_number_container">
             <?php for ($i = max(1, $currentPage - $pageLinkRange); $i <= min($totalPages, $currentPage + $pageLinkRange); $i++) : ?>
@@ -23,7 +28,11 @@ $pageLinkRange = 3;
             <?php endfor ?>
         </div>
         <?php if ($currentPage !== $totalPages) : ?>
-            <a class="to_last_page" href='<?php echo "result?search_words=$searchWordString&検索開始=送信&page=$totalPages&display_count=$displayCount" ?>'>&gt;&gt;</a>
+            <!-- 複数ページぶん戻る -->
+            <?php $skipPagesToUpper = min($currentPage + $skipPagesStep, $totalPages) ?>
+            <a class="to_lesser symbol" href=<?php echo "result?search_words=$searchWordString&検索開始=送信&page=$skipPagesToUpper&display_count=$displayCount" ?>>&gt;</a>
+            <!-- 最初のページへ -->
+            <a class="to_first_page symbol" href='<?php echo "result?search_words=$searchWordString&検索開始=送信&page=$totalPages&display_count=$displayCount" ?>'>&gt;&gt;</a>
         <?php endif ?>
     <?php endif ?>
 </div>
