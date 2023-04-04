7dceb627bf6081dcfcf7172f4529c34f88b9b18a
testuser <canda3267@gmail.com>
2023-04-04 22:59:08 +0900

    [fix]最初ページ、最後ページに戻るリンクを削除(使わないため)

--- app/view/pagination.php
+++ app/view/pagination.php
@@ -9,18 +9,12 @@ $totalPages = (new TotalResultPagesCalculator($searchResult, $displayCount))->ge
 
 <div class="pagination_container">
     <?php if ($totalPages >= 2) : ?>
-        <?php if ($currentPage !== 1) : ?>
-            <a href=<?php echo "result?search_words=$searchWordString&検索開始=送信&page=1&display_count=$displayCount" ?>>&lt;&lt;</a>
-        <?php endif ?>
         <?php for ($i = 1; $i <= $totalPages; $i++) : ?>
             <?php if ($i === $currentPage) : ?>
-                <a><?php echo $i ?></a>
+                <a class="this_page_number"><?php echo $i ?></a>
             <?php else : ?>
-                <a href=<?php echo "result?search_words=$searchWordString&検索開始=送信&page=$i&display_count=$displayCount" ?>><?php echo $i ?></a>
+                <a class="other_page_number" href='<?php echo "result?search_words=$searchWordString&検索開始=送信&page=$i&display_count=$displayCount" ?>'><?php echo $i ?></a>
             <?php endif ?>
         <?php endfor ?>
-        <?php if ($currentPage !== $totalPages) : ?>
-            <a href=<?php echo "result?search_words=$searchWordString&検索開始=送信&page=$totalPages&display_count=$displayCount" ?>>&gt;&gt;</a>
-        <?php endif ?>
     <?php endif ?>
 </div>
