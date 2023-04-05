8f827fb186ff3038802d805b9c76eea9e3eb8df1
testuser <canda3267@gmail.com>
2023-04-06 00:35:05 +0900

    [fix]display_numberをdisplay_countに変更

--- app/view/displayCountDecisionView.php
+++ app/view/displayCountDecisionView.php
@@ -1,7 +1,7 @@
 <div class="display_count_container">
     <p>表示数調整</p>
     <form>
-        <select id="display_number">
+        <select id="display_count">
             <option value="25">25</option>
             <option value="50">50</option>
             <option value="100">100</option>
